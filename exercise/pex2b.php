06dc5e18cbef3f04dc42f67db60dad366651d973
Mitchell Mosure <hctimit@gmail.com>
2019-11-11 12:45:24 -0600

    feat: pex2

---
+++ exercise/pex2b.php
@@ -0,0 +1,6 @@
+<?php
+    $input=$_POST['txtMsg'];
+    echo "You said: $input";
+    echo "You said: ".$input; // Here I am using concatenation or joining of strings
+    // I prefer this method
+?>
