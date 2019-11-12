5e9c997c136262f8c9ef924b5cdb3d3375d227f8
Mitchell Mosure <hctimit@gmail.com>
2019-11-12 13:03:45 -0600

    feat: pex3e

---
+++ exercise/pex3e.php
@@ -0,0 +1,18 @@
+<html>
+    <head> </head>
+    <body>
+        <form action= "pex3f.php" method="post">
+            Enter the number of rows:
+            <input type="nummber" name="txtRows" size="30">
+
+            <br/>
+
+            Enter the number of columns:
+            <input type="nummber" name="txtColumns" size="30">
+
+            <br/>
+            
+            <input type="submit" value="Send">
+        </form>
+    </body>
+</html>
---
+++ exercise/pex3f.php
@@ -0,0 +1,22 @@
+<?php
+    $rows = trim($_POST['txtRows']);
+    $columns = trim($_POST['txtColumns']);
+
+    if (is_int($rows) && is_int($columns)) {
+        echo "<table border=1>";
+
+        for ($i = 0; $i < $rows; $i++) {
+            echo "<tr>";
+
+            for ($j = 0; $j < $columns; $j++) {
+                echo "<td/>";
+            }
+
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Invalid input.";
+    }
+?>
