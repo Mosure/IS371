517fc466c997f7b3acbcc6e88a6aa9d041187289
Mitchell Mosure <hctimit@gmail.com>
2019-11-12 13:00:10 -0600

    feat: point to the correct php file

--- exercise/pex3c.php
+++ exercise/pex3c.php
@@ -1,7 +1,7 @@
 <html>
     <head> </head>
     <body>
-        <form action= "pex3b.php" method="post">
+        <form action= "pex3d.php" method="post">
             Enter the number of times to say hello:
             <input type="nummber" name="txtNum" size="30">
 
