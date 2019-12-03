84773d228dc76ea6cf42f9f67fd3712a26cb481b
Mitchell Mosure <hctimit@gmail.com>
2019-12-03 13:55:38 -0600

    fix: table error

--- php/pex8/update.php
+++ php/pex8/update.php
@@ -123,12 +123,10 @@
             if ($_POST['add_major_post']) {
                 $maj_id = $_POST['add_major'];
 
-                $sql_addMaj = "INSERT INTO alum_majors VALUES ('$id', '$maj_id')";
+                $sql_addMaj = "INSERT INTO alum_major VALUES ('$id', '$maj_id')";
 
                 $result = mysqli_query($conn, $sql_addMaj);
 
-                echo mysqli_error($conn);
-                
                 if (!$result) {
                     die("cannot processed insert query");
                 }
@@ -137,11 +135,9 @@
             if ($_POST['delete_major_post']) {
                 $maj_id = $_POST['add_major'];
 
-                $sql_delMaj = "DELETE FROM alum_majors WHERE alum_id='$id' AND major_id='$maj_id'";
+                $sql_delMaj = "DELETE FROM alum_major WHERE alum_id='$id' AND major_id='$maj_id'";
 
                 $result = mysqli_query($conn, $sql_delMaj);
-
-                echo mysqli_error($conn);
 
                 if (!$result) {
                     die("cannot processed delete query");
