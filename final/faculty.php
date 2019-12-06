45d529d80ef9bfe8a69db38420bfea816c20eef7
Mitchell Mosure <hctimit@gmail.com>
2019-12-06 16:09:06 -0600

    feat: added frame

--- final/faculty.php
+++ final/faculty.php
@@ -52,7 +52,7 @@
                         $num = mysqli_num_rows($result);
 
                         if ($num > 0) {
-                            echo "<table style='width: 100%;'>";
+                            echo "<table style='width: 100%;' frame='box'>";
 
                             $i = 0;
 
