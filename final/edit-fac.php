422e44ef674f9cbcd82108f5b457c4d2cace3a27
Mitchell Mosure <hctimit@gmail.com>
2019-12-06 18:04:36 -0600

    fix: ;

--- final/edit-fac.php
+++ final/edit-fac.php
@@ -299,7 +299,7 @@
                             } elseif ($_POST["updatePost"]) {
                                 $get_user_id = "SELECT * FROM faculty WHERE fac_id = '$id'";
                                 $get_result = mysqli_query($conn, $get_user_id);
-                                $row = mysqli_fetch_assoc($get_result)
+                                $row = mysqli_fetch_assoc($get_result);
                                 $user_id = $row["uid"];
 
                                 $position = $_POST['position'];
